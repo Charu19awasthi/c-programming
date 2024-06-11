138dbb2652d60bf41ffb51a1e5cf1caccb314b9d
CHARU AWASTHI <148922861+Charu19awasthi@users.noreply.github.com>
2024-06-11 21:42:10 +0530

    Add files via upload

---
+++ asciii.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int i;
+    for (i=65; i<=90;i++)
+    {
+        printf("%d=%c\n",i,i);
+    }
+    for (i=97;i<=122;i++)
+    {
+        printf("%d=%c\n",i,i);
+
+    }
+    getch();
+}
